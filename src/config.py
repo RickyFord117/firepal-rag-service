0445a453ef5711296dfad25683eb663f6f493e67
rick <rg.henriques@hotmail.com>
2025-07-22 02:07:59 +0100

    neo4J and langchain packages

--- src/config.py
+++ src/config.py
@@ -23,3 +23,8 @@ VECTOR_SIZE = 1536
 
 # --- SQL Database config ---
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./polidex.db")
+
+# --- Neo4j config ---
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
